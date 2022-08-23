cc6f4f29e1f847d3091f6830eac6a1a4a5cf65ca
isaaclee12 <isaac.lee@uvm.edu>
2022-08-23 00:43:10 -0400

    Figured out how to implement pages and layouts. Added tailwindcss.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<!-- This file's contents apply to every page -->
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About Me</a>
+    <a href="/resume">Resume</a>
+    <a href="/projects">Project List</a>
+    <!-- Uncomment the below line once the project is finished -->
+    <!-- <a href="/latest-project">Latest Project</a> -->
+    <a href="/contact">Contact</a>
+</nav>
+
+<slot></slot>
+
+<footer>
+    <p>This is a footer.</p>
+</footer>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,1 @@
+<h1>i did everything</h1>
