8d78d89832141309f602e3c5376e9d97cfaa2144
isaaclee12 <isaac.lee@uvm.edu>
2022-09-01 00:14:34 -0400

    Web server finally runs on my pi, adding a draft for a new nav.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,32 @@
 <!-- This file's contents apply to every page -->
 
+
+
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About Me</a>
-    <a href="/resume">Resume</a>
-    <a href="/projects">Project List</a>
-    <!-- Uncomment the below line once the project is finished -->
-    <!-- <a href="/latest-project">Latest Project</a> -->
-    <a href="/contact">Contact</a>
+    <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+        <a class="text-xl text-black" href="#">Navbar</a>
+        <!-- Left links -->
+        <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About Me</a>
+            </li>
+            <li>
+                <a href="/resume">Resume</a>
+            </li>
+            <li>
+                <a href="/projects">Project List</a>
+            </li>
+            <!-- Uncomment the below line once the project is finished -->
+            <!-- <a href="/latest-project">Latest Project</a> -->
+            <li>
+                <a href="/contact">Contact</a>        
+            </li>
+        </ul>
+        <!-- Left links -->
+    </div>
 </nav>
 
 <slot></slot>
