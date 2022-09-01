51b0116dd1c18544641afd1c95afa98cff3c3353
isaaclee12 <isaac.lee@uvm.edu>
2022-09-01 01:52:07 -0400

    tailwind. again.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,svelte,js,ts}"],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
