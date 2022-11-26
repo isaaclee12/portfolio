7783d1cbe182830d769266290b193782985d3462
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2022-11-26 10:23:36 -0500

    Added template + svgs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,44 +5,153 @@
     import "../app.css";
 </script>
 
-<!-- Nav bar -->
-<nav class = "relative w-full flex flex-wrap items-center justify-between py-4 bg-gray-100 shadow-lg sticky top-0">
-    <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-        <div class="collapse navbar-collapse flex-grow items-center space-x-6" id="navbarSupportedContent">
-            <a class="text-xl text-black" href="/">Isaac Lee</a>
-            <!-- Left links -->
-            <a href="/" class="text-gray-500 hover:text-gray-600 focus:text-gray-600">Home</a>
-            <a href="/about" class="text-gray-500 hover:text-gray-600 focus:text-gray-600">About Me</a>
-            <a href="/RESUME" class="text-gray-500 hover:text-gray-600 focus:text-gray-600">Resume</a>
-            <a href="/projects" class="text-gray-500 hover:text-gray-600 focus:text-gray-600">Project List</a>
-            <a href="/contact" class="text-gray-500 hover:text-gray-600 focus:text-gray-600">Contact</a>        
+	<body class="is-preload">
 
-            <!-- <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About Me</a>
-                </li>
-                <li>
-                    <a href="/resume">Resume</a>
-                </li>
-                <li>
-                    <a href="/projects">Project List</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>        
-                </li>
-            </ul> -->
-            <!-- Left links -->
-        </div>
-        <!-- Collapsible wrapper -->
-    </div>
-</nav>
+		<!-- Header -->
+			<section id="header">
+                <!-- <h1>
+                    <strong>Isaac Lee</strong> is a Full Stack Software Engineer with an <br />
+                    enthusiastic drive for creating incredible products.
+                </h1> -->
+                <!-- <i class="fa-solid fa-user"></i> -->
+				<div class="inner">
+					<!-- <span class="icon solid major fa-cloud"></span> -->
+					<h1>
+						<strong>Isaac Lee</strong> is a Full Stack Software Engineer with an <br />
+						enthusiastic drive for creating incredible products.
+					</h1>
+					<ul class="actions special">
+						<li><a href="#one" class="button scrolly">Discover</a></li>
+					</ul>
+				</div>
+			</section>
 
-<slot></slot>
+		<!-- One -->
+			<section id="one" class="main style1">
+				<div class="container">
+					<div class="row gtr-150">
+						<div class="col-6 col-12-medium">
+							<header class="major">
+								<h2>A Self-Starter, and a Go-Getter</h2>
+							</header>
+							<p>
+								Planned, managed, and developed diverse projects. Mastered broad domain knowledge, technologies, and their uses.
+								Demonstrated ability to adapt on the fly. Achieved and exceeded several product goals for different clients.
+							</p>
 
-<footer>
-    <!-- <p>This is a footer.</p> -->
-    <p class = "my-20"></p>
-</footer>
+						</div>
+						<div class="col-6 col-12-medium imp-medium">
+							<span class="image fit"><img src="assets/me_coding.gif" alt="" /></span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main style2">
+                                <!-- <img src="assets/react-icon.svg" alt="react-icon"/> -->
+				<div class="container">
+					<div class="row gtr-150">
+						<div class="col-6 col-12-medium">
+							<ul class="major-icons">
+								<li><span class="icon">
+                                    <img src="assets/react-icon.svg" alt="react-icon"/>
+                                </span></li>
+								<li><span class="icon">
+                                    <img src="assets/github-icon.svg" alt="github-icon"/>
+                                </span></li>
+								<li><span class="icon solid style3 major"></span></li>
+								<li><span class="icon solid style4 major"></span></li>
+								<li><span class="icon solid style5 major"></span></li>
+								<li><span class="icon solid style6 major"></span></li>
+							</ul>
+						</div>
+						<div class="col-6 col-12-medium">
+							<header class="major">
+								<h2>Master of Technologies, from Front-End to Back-End</h2>
+							</header>
+							<p>Mastered broad domain knowledge, technologies, and their uses. Demonstrated ability to adapt on the fly.</p>
+							<ul>
+								<li>Languages:	Python, C++, JavaScript, TypeScript, HTML, CSS</li>
+								<li>Utilities:	Git, Github, React, Django, Node, npm</li>
+								<li>Databases: SQL, MySQL, SQLite3</li>
+								<li>Infrastructure:	AWS, Azure, Docker, Linux, Cloudflare, Nginx</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="main style1 special">
+				<div class="container">
+					<header class="major">
+						<h2>Projects</h2>
+					</header>
+					<p>I've completed several solo and collaborative projects.</p>
+					<div class="row gtr-150">
+						<div class="col-4 col-12-medium">
+							<span class="image fit"><img src="assets/pic02.jpg" alt="" /></span>
+							<h3>Magna feugiat lorem</h3>
+							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+							<ul class="actions special">
+								<li><a href="#" class="button">More</a></li>
+							</ul>
+						</div>
+						<div class="col-4 col-12-medium">
+							<span class="image fit"><img src="assets/pic03.jpg" alt="" /></span>
+							<h3>Magna feugiat lorem</h3>
+							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+							<ul class="actions special">
+								<li><a href="#" class="button">More</a></li>
+							</ul>
+						</div>
+						<div class="col-4 col-12-medium">
+							<span class="image fit"><img src="assets/pic04.jpg" alt="" /></span>
+							<h3>Magna feugiat lorem</h3>
+							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+							<ul class="actions special">
+								<li><a href="#" class="button">More</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Four -->
+			<section id="four" class="main style2 special">
+				<div class="container">
+					<header class="major">
+						<h2>Ipsum feugiat consequat?</h2>
+					</header>
+					<p>Sed lacus nascetur ac ante amet sapien.</p>
+					<ul class="actions special">
+						<li><a href="#" class="button wide primary">Sign Up</a></li>
+						<li><a href="#" class="button wide">Learn More</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
